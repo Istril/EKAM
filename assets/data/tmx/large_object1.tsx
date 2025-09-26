e0905c52be3a2af5917ffa789fed77485ea37a83
Istril <nightfear_withoutfeelings@bk.ru>
2025-09-26 20:28:50 +0300

    more changes: Alchemy; skill description (rus), start create meltorra

---
+++ assets/data/tmx/large_object1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="icemist_object" tilewidth="64" tileheight="128">
+ <image source="large_object.png" width="640" height="512"/>
+</tileset>
---
+++ assets/data/tmx/teleporter2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="teleporter2" tilewidth="192" tileheight="96">
+ <image source="teleporter2.png" width="192" height="96"/>
+</tileset>
